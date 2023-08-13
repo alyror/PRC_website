6e87a47867ed8d534d3943845920f6c3c857b3dc
vergsjc <vergarajhoncarl@gmail.com>
2023-08-13 19:13:56 +0800

    commit vergara

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>
-        <?php if(isset($page_title)){echo "$page_title";} ?> - Philippine Red Criss
+        <?php if(isset($page_title)){echo "$page_title";} ?> - Philippine Red Cross
     </title>
     <link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
     <link href="//fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
@@ -15,8 +15,64 @@
 
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 
+ 
 
     
 </head>
+
+
 <body>
+
+<header >
+<div class="container p-0 m-0">
+    <div class="row  d-flex flex-row ">
+        <div class="col-sm-1 logo">
+            <img src="images/prc-logo.png" class="img-fluid" alt="PRC Logo">
+        </div>
+        <div class="col-sm-4 containertext">
+            <div class="logo-text">
+                PHILIPPINE RED CROSS
+            </div>
+            <div class="chapter-text">
+                BATANGAS CHAPTER
+            </div>
+        </div>
+    </div>
+</div>
+
+
+  </header>
+
+<style>
+
+  
+       header {
+            background-color: #f2f2f2; /* Header background color */
+       padding: 10px 20px; /* Adjust padding for top and bottom */
+
+            display: flex; /* Use flexbox for layout */
+            align-items: center; /* Center vertically */
+        }
+    
+       
+        .logo img {
+            max-width: 60px; /* Adjust the max-width for the logo */
+            height: auto; /* Maintain aspect ratio */
+            margin-right: 10px; /* Add space between logo and text */
+        }
+     
+        .logo-text {
+            font-size: 18px; /* Adjust font size */
+            font-weight: bold; /* Apply bold styling */
+            margin-bottom: 2px; /* Add space between texts */
+            color: #030154;font-family: "Poppins", sans-serif; 
+        }
+        .chapter-text {
+            font-size: 18px; /* Adjust font size for chapter text */
+            font-weight: bold;
+            color: #CF0006;font-family: "Poppins", sans-serif; 
+        }
+
+</style>
+
 
